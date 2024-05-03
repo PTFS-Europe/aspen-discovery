c222554f8c9fe30b23b965b0a8ad79f59d59d1d1
Alexander Blanchard <alexander.blanchard@ptfs-europe.com>
2024-05-03 11:54:11 +0100

    feat: Add ability to enable or disable Summon Book Covers
    This commit adds a setting in Library Systems to enable available book covers in Summon to
    be displayed or to use only generated covers as this impacts loading time.

--- code/web/sys/LibraryLocation/Library.php
+++ code/web/sys/LibraryLocation/Library.php
@@ -354,6 +354,7 @@ class Library extends DataObject {
 
 	//Summon Settings
 	public $summonSettingsId;
+	public $showAvailableCoversInSummon;
 
 	//SSO
 	public /** @noinspection PhpUnused */
@@ -3672,7 +3673,6 @@ class Library extends DataObject {
 				'label' => 'Summon',
 				'hideInLists' => true,
 				'renderAsHeading' => true,
-				// 'permissions' => ['A'],
 				'properties' => [
 					'summonSettingsId' => [
 						'property' => 'summonSettingsId',
@@ -3682,6 +3682,14 @@ class Library extends DataObject {
 						'description' => 'Whether or not Summon content should be included for this library.',
 						'hideInLists' => true,
 						'default' => -1,
+					],
+					'showAvailableCoversInSummon' => [
+						'property' => 'showAvailableCoversInSummon',
+						'type' => 'checkbox',
+						'label' => 'Show Available Covers in Summon',
+						'description' => 'Determine whether or not available book covers should be displayed in Summon',
+						'hideInLists' => true,
+						'default' => 0,
 					],
 				],
 			],
@@ -4920,6 +4928,7 @@ class Library extends DataObject {
 			'passwordLabel' => $this->loginFormPasswordLabel ?? 'Library Card Number',
 			'code' => $this->ilsCode,
 			'finePaymentType' => (int)$this->finePaymentType,
+			'showAvailableCoversInSummon' => $this->showAvailableCoversInSummon,
 		];
 		if (empty($this->baseUrl)) {
 			$apiInfo['baseUrl'] = $configArray['Site']['url'];

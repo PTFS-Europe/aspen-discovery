25b85d60efe0141e65eea0007b53b6518fdd99a2
Alexander Blanchard <alexander.blanchard@ptfs-europe.com>
2024-06-24 16:52:07 +0100

    feat: Create Objects and DB Tables
    Create Objects and DB tables for community campaigns and milestones.

---
+++ code/web/sys/Community/Campaign.php
@@ -0,0 +1,25 @@
+<?php
+class Campaign extends DataObject {
+    public $__table = 'campaign';
+    public $id;
+    public $name;
+
+    public static function getObjectStructure($context = ''): array {
+        return [
+            'id' => [
+                'property' => 'id',
+                'type' => 'label',
+				'label' => 'Id',
+				'description' => 'The unique id',
+            ],
+            'name' => [
+				'property' => 'name',
+				'type' => 'text',
+				'label' => 'Name',
+				'maxLength' => 50,
+				'description' => 'A name for the campaign',
+				'required' => true,
+			],
+        ];
+    }
+}
--- code/web/sys/DBMaintenance/community_engagement_updates.php
+++ code/web/sys/DBMaintenance/community_engagement_updates.php
@@ -9,5 +9,25 @@ function getCommunityEngagementUpdates() {
 				"INSERT INTO modules (name, indexName, backgroundProcess) VALUES ('Community', '', '')",
 			],
 		],
+        'create_campaigns' => [
+            'title' => 'Create Campaigns',
+            'description' => 'Add table for campaigns',
+            'sql' => [
+                "CREATE TABLE campaign (
+                    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                    name VARCHAR(100) NOT NULL
+                ) ENGINE = InnoDB",
+            ],
+        ],
+        'create_milestones' => [
+            'title' => 'Create Milestones',
+            'description' => 'Add table for milestones',
+            'sql' => [
+                "CREATE TABLE milestone (
+                    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                    name VARCHAR(100) NOT NULL
+                ) ENGINE = InnoDB",
+            ],
+        ],
     ];
 }

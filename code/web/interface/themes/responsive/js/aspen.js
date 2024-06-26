ff187a72ea09640800a4fbbe4887792409379668
Alexander Blanchard <alexander.blanchard@ptfs-europe.com>
2024-06-26 09:14:30 +0100

    feat: Add campaigns to account
    Add a My Campaigns section to the user account
    
    feat: Add Your Campaigns to Account
    
    feat: Alter display in My Campaigns

--- code/web/interface/themes/responsive/js/aspen.js
+++ code/web/interface/themes/responsive/js/aspen.js
@@ -8243,6 +8243,11 @@ AspenDiscovery.Account = (function () {
 					return AspenDiscovery.Account.markILSMessageAsUnread(id);
 				}, false);
 			}
+		},
+		seeCampaigns: function () {
+			var url = Globals.path + "/MyAccount/AJAX?method=seeAvailableCampaigns";
+			document.location.href = url;
+			return false;
 		}
 	};
 }(AspenDiscovery.Account || {}));

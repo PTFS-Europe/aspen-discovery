d460947f702d4f967af4da9ce0a450e7bfde8d58
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-08-14 13:42:16 +0100

    feat: GET ILL request list from  Koha
    Consumes the Koha api to fetch a list of ILL requests made by a specific
    patron.

--- code/web/services/ILL/MyRequests.php
+++ code/web/services/ILL/MyRequests.php
@@ -4,6 +4,21 @@ require_once ROOT_DIR . '/services/MyAccount/MyAccount.php';
 class ILL_MyRequests extends MyAccount {
 
 	function launch() {
+	}
+
+	private function getRequestsBy($user) {
+		$catalog = CatalogFactory::getCatalogConnectionInstance();
+		$reqUrl = $catalog->getStaffClientBaseURL() . '/api/v1/ill/requests' . "?q={\"patron_id\":" . ($user->unique_ils_id) . "}";
+
+		$credentials = $user->ils_username . ':' . $user->ils_password; // $user->ils_password is likely to be null -> TODO: need to find a way to safely get and pass this credential
+
+		// fetch the ILL requests made by the user
+		$curl = curl_init($reqUrl);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curl, CURLOPT_HEADER, 1);
+		curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt($curl, CURLOPT_USERPWD, $credentials);
+		$response = curl_exec($curl);
 
 	}
 

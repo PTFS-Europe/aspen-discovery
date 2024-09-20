42ed62a232f36388f80e33e0105a76c18b9a179e
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2024-09-20 11:54:20 +0000

    squash to 02537aa17a4b34a52f280e0cdd9571471db0194d
    These 2 lines replace the need for cron. Insane

--- code/web/sys/Community/Milestone.php
+++ code/web/sys/Community/Milestone.php
@@ -167,6 +167,9 @@ class Milestone extends DataObject {
                 "milestoneUsersProgress" => $milestoneUsersProgress
             ]
         );
+
+        $milestoneUsersProgress->progress++;
+        $milestoneUsersProgress->update();
     }
 
     /**
